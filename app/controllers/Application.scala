37bbb2b4540a87814a8fcf84f1c7ef87304086d1
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-12 01:26:37 +0100

    400 status code on malformed query + remove unused imports

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,23 +3,22 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 import scalex.search.{ Engine, RawQuery, Results }
-import org.jboss.netty.handler.codec.http.QueryStringDecoder
-import scala.collection.JavaConversions._
 
-import scalaz.{Failure, Success}
+import scalaz.{ Failure, Success }
 
 object Application extends Controller {
 
   lazy val env = new ScalexHttpEnv
   lazy val engine = env.scalexEnv.engine
 
-  def index(q: String, page: Int) = Action { request =>
-      (engine find RawQuery(q, page, 15) match {
-        case Failure(e) => Ok(e)
-        case Success(r) => Ok(r.defs.mkString("<br />"))
-      }).as("text/html")
+  def index(q: String, page: Int) = Action { request ⇒
+    (engine find RawQuery(q, page, 15) match {
+      case Failure(e) ⇒ BadRequest(e)
+      case Success(r) ⇒ Ok(r.defs.mkString("<br />"))
+    }).as("text/html")
 
   }
 
