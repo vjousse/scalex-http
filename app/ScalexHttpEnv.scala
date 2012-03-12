274fc1e79d9217d4b40ac9c51aa4488bc9db1249
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-12 01:26:16 +0100

    Fix scalex env instanciation

--- app/ScalexHttpEnv.scala
+++ app/ScalexHttpEnv.scala
@@ -2,7 +2,5 @@ package scalex.http
 
 class ScalexHttpEnv {
 
-  val scalexEnv = new scalex.Env(Map(
-    "index_file" -> "/home/vjousse/usr/src/scala/scalex/index.dat"
-  ))
+  val scalexEnv = scalex.Env("/etc/scalex.conf")
 }
