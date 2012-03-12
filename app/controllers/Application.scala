0a614adf8b071b2ab5943090f3c4f10b5170e058
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-12 01:46:40 +0100

    Implement caching and response delivery

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,19 +7,29 @@ import play.api.libs.json._
 
 import scalex.search.{ Engine, RawQuery, Results }
 
-import scalaz.{ Failure, Success }
+import collection.mutable.WeakHashMap
+import scalaz.{ Validation, Failure, Success }
 
 object Application extends Controller {
+
+  type Result = Validation[String, String]
 
   lazy val env = new ScalexHttpEnv
   lazy val engine = env.scalexEnv.engine
 
-  def index(q: String, page: Int) = Action { request ⇒
-    (engine find RawQuery(q, page, 15) match {
-      case Failure(e) ⇒ BadRequest(e)
-      case Success(r) ⇒ Ok(r.defs.mkString("<br />"))
-    }).as("text/html")
+  val limit = 15
+  val cache = WeakHashMap[RawQuery, Result]()
 
+  def index(q: String, page: Int) = Action { request ⇒
+    val query = RawQuery(q, page, limit)
+    val result = cache.getOrElseUpdate(query, search(query))
+    (result match {
+      case Failure(e) ⇒ BadRequest(e)
+      case Success(r) ⇒ Ok(r)
+    }).as("application/json")
   }
 
+  def search(query: RawQuery): Result = engine find query map {
+    case Results(paginator, defs) ⇒ Formatter(query.string, paginator, defs) toString
+  }
 }
