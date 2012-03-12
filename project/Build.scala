d2f384cd10fe593a8c22524ba24dfa1010a6b07b
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-03-12 01:27:08 +0100

    Remove dependency to play2-mini

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,6 @@ trait Resolvers {
 }
 
 trait Dependencies {
-  val playmini = "com.typesafe" %% "play-mini" % "2.0-RC4"
   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0.4"
   val specs2 = "org.specs2" %% "specs2" % "1.8.2"
 }
